6d0cfdaeae1bc17be7dc6ea240b42609e574d7e4
LVas90 <pasx23@yahoo.gr>
2025-06-19 16:39:29 +0300

    Create main.cs

---
+++ main.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collection.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespacce CSharpTutorials
+{
+    class Program
+    {    
+        static void Main(string[] args)
+         {
+            string message = "Hello World!";
+
+            Console.Writeline(message);
+         }
+    }
+}
